10413a64e2097f1591b6f116807abb094a704a45
Felix Kipkorir <kipronofelix63@gmail.com>
2023-03-27 05:43:34 -0700

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * strlen - returns the length of a string.
+ *
+ * Return: 0.
+ */
+int _strlen(char *s)
+{
+s = "Hello Korir";
+int len;
+
+len = strlen(s);
+printf("d\n", len);
+return 0;
+}
