c6130d7b25ebbbe1d26b020694b8a12bb35f8f48
Felix Kipkorir <kipronofelix63@gmail.com>
2023-04-03 06:19:12 -0700

    0x07. C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,12 +7,12 @@
  * @b: desired value
  *
  * Return: modified array with new value of n bytes
- */ 
-char *_memset(char *s,char b,unsigned int n)
+ */
+char *_memset(char *s, char b, unsigned int n)
 {
 int a = 0;
 
-for(;n > 0;a++)
+for ( ; n > 0; a++)
 {
 s[a] = b;
 n--;
