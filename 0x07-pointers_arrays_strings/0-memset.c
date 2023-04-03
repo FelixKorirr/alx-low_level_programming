cbc39bbe974f2284378d9d2fcd863d0cc10ff317
Felix Kipkorir <kipronofelix63@gmail.com>
2023-04-03 06:01:37 -0700

    0x07. C - Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: address of memory to be filled
+ * @n: number of bytes to be changed
+ * @b: desired value
+ *
+ * Return: modified array with new value of n bytes
+ */ 
+char *_memset(char *s,char b,unsigned int n)
+{
+int a = 0;
+
+for(;n > 0;a++)
+{
+s[a] = b;
+n--;
+}
+return (s);
+}
