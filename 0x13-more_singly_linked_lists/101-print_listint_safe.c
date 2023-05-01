3131246e2efbb7d7214c3ea8db42817b7c031b18
Felix Kipkorir <kipronofelix63@gmail.com>
2023-05-01 06:52:23 -0700

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -8,7 +8,7 @@ size_t print_listint_safe(const listint_t *head);
  * looped_listint_len - Counts the number of unique nodes
  * in a looped listint_t linked list.
  * @head: A pointer to the head of the listint_t to check.
- * Return: 0 if list is not looped,else, the number of unique nodes in the list.
+ * Return: 0 if list is not looped,else,the number of unique nodes in the list
  */
 
 size_t looped_listint_len(const listint_t *head)
