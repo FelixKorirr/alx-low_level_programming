407c7a62e711af4eb3e561f8029a48dbe81a6302
Felix Kipkorir <kipronofelix63@gmail.com>
2023-05-02 07:57:07 -0700

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,40 +1,88 @@
 #include "lists.h"
 
+size_t looped_listint_count(listint_t *head);
+size_t free_listint_safe(listint_t **h);
+
 /**
- * free_listint_safe - frees a listint_t list
- * @h: pointer to the first node in listint_t list
- *
- * Return: size of the list that was freed
+ * looped_listint_count - Counts the number of nodes                  in a looped listint_t linked list.
+ * @head: pointer to the head of the listint_t
+ * Return: 0 if list is not looped else number of nodes in the list
+ */
+size_t looped_listint_count(listint_t *head)
+{
+	const listint_t *t, *s;
+	size_t node = 1;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+
+	t = head->next;
+	s = (head->next)->next;
+
+	while (s)
+	{
+		if (t == s)
+		{
+			t = head;
+			while (t != s)
+			{
+				node++;
+				t = t->next;
+				s = s->next;
+			}
+
+			t = s->next;
+			while (t != s)
+			{
+				node++;
+				t = t->next;
+			}
+
+			return (node);
+		}
+
+		t = t->next;
+		s = (s->next)->next;
+	}
+
+	return (0);
+}
+
+/**
+ * free_listint_safe - frees the listint_t list safely                  can free lists containing loops)
+ * @h: pointer to the head of the listint_t list.
+ * Return: size of the freed list.
  */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t x = 0;
-	listint_t *p;
-	int y;
+	listint_t *x;
+	size_t node, indx;
 
-	if (!h || !*h)
-		return (0);
+	node = looped_listint_count(*h);
 
-	while (*h)
+	if (node == 0)
 	{
-		y = *h - (*h)->next;
-		if (y > 0)
+		for (; h != NULL && *h != NULL; node++)
 		{
-			p = (*h)->next;
+			x = (*h)->next;
 			free(*h);
-			*h = p;
-			x++;
-		}
-		else
-		{
-			free(*h);
-			*h = NULL;
-			x++;
-			break;
+			*h = x;
 		}
 	}
 
-	*h = NULL;
+	else
+	{
+		for (indx = 0; indx < node; indx++)
+		{
+			x = (*h)->next;
+			free(*h);
+			*h = x;
+		}
 
-	return (x);
+		*h = NULL;
+	}
+
+	h = NULL;
+
+	return (node);
 }
